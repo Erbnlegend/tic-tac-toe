15a223d4174ea3c2aef83943f8a448389033fc7c
Aaron Erb <erbnlegend@gmail.com>
2022-11-05 16:25:57 -0500

    Add Player Logic for Game

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,14 +7,16 @@
 const Game = (() => {
     const board = document.getElementById('board')
     const boxes = [...document.querySelectorAll('.playArea')]
+    const wins = document.getElementById('wins')
     const info = document.querySelector('.player')
-    const gameBoard = [
+    const resetButton = document.querySelector('.resetInfo')
+    const resetEvent = document.getElementById('reset')
+    resetEvent.addEventListener('click', reset)
+    let gameBoard = [
         ['','',''],
         ['','',''],
         ['','','']
     ]
-    
-    let winner = ""
 
     boxes.forEach(element => {
         const coords = JSON.parse(element.dataset.coordinates)
@@ -24,73 +26,125 @@ const Game = (() => {
         
     })
     
-    const PlayerPlays = (player, mark, turn) => {
+    const PlayerPlays = (player, mark) => {
         return {
             player,
-            mark,
-            turn
+            mark
         }
     }
-    const player1 = PlayerPlays('Player X', 'X', true)
-    const player2 = PlayerPlays('Player O', 'O', false)
+    const player1 = PlayerPlays('Player X', 'X')
+    const player2 = PlayerPlays('Player O', 'O')
     let currentPlayer = player1
     info.textContent = currentPlayer.player
+    // Init number of Rounds for winner
+    let rounds = 0
+    let winner = false
 
 // Will change on player Selections
     
     function play(element, coords) {
-        info.classList.toggle('animate')
-        if(gameBoard[coords.x][coords.y] === player1.mark || gameBoard[coords.x][coords.y] === player2.mark) {
-            info.textContent = "yOu hErE tRyN CHeAt " + currentPlayer + "???"
-        }
-        else if(player1.turn) {
-            element.textContent = player1.mark
-            player1.turn = false
-            player2.turn = true
-            gameBoard[coords.x][coords.y] = player1.mark
-
-        }
-        else if(player2.turn) {
-            element.textContent = player2.mark
-            player1.turn = true
-            player2.turn = false
-            gameBoard[coords.x][coords.y] = player2.mark
-        }
-        setTimeout(() => {
+        if(!winner) {
+            wins.textContent = "No Winner Quite Yet"
             info.classList.toggle('animate')
-        }, 1);
-        checkWin(gameBoard)
-        if(currentPlayer === player1) {
-            currentPlayer = player2
+            // No Cheaters
+            if(gameBoard[coords.x][coords.y] === player1.mark || gameBoard[coords.x][coords.y] === player2.mark) {
+                info.textContent = "yOu hErE tRyN CHeAt " + currentPlayer.player + "???"
+                setTimeout(() => {
+                    info.textContent = `Try again ${currentPlayer.player}`
+                }, 2000);
+            }
+            else if(currentPlayer === player1) {
+                element.textContent = player1.mark
+                element.style.backgroundColor = 'rgba(94, 151, 61, 0.596)'
+                gameBoard[coords.x][coords.y] = currentPlayer.mark
+                // Must check for win before currentPlayer changes
+                checkWin(gameBoard)
+                currentPlayer = player2
+                setTimeout(() => {
+                    info.innerHTML = currentPlayer.player
+                }, 1);
+            }
+            else if(currentPlayer === player2) {
+                element.textContent = currentPlayer.mark
+                element.style.backgroundColor = 'rgba(61, 103, 151, 0.596)'
+                gameBoard[coords.x][coords.y] = currentPlayer.mark
+                // Must check for win before currentPlayer changes
+                checkWin(gameBoard)
+                currentPlayer = player1
+
+                setTimeout(() => {
+                    info.innerHTML = currentPlayer.player
+                }, 1);
+            }
+            setTimeout(() => {
+                info.classList.toggle('animate')
+            }, 1);
         }
-        else if(currentPlayer === player2) {
-            currentPlayer = player1
-        }
-        setTimeout(() => {
-            info.innerHTML = currentPlayer.player
-        }, 1);
     }
 
     function checkWin(gameBoard) {
-        const checkPlayer1 = (current) => current === player1.mark
-        const checkPlayer2 = (current) => current === player2.mark
+        if(!winner) {
+            // increment rounds for tie game check
+            rounds++
+            const checkPlayer = (current) => current === currentPlayer.mark
+            const getColumn1 = gameBoard.map(i => i[0])
+            const getColumn2 = gameBoard.map(i => i[1])
+            const getColumn3 = gameBoard.map(i => i[2])
 
-        for(let item in gameBoard) {
-            if(gameBoard[item].every(checkPlayer1)) {
-                info.textContent = `${currentPlayer.player} Wins!`
+            // Each Row
+            for(let item in gameBoard) {
+                if(gameBoard[item].every(checkPlayer)) {
+                    wins.textContent = `${currentPlayer.player} Wins!`
+                    winner = true
+                    console.log(gameBoard[item])
+                }
             }
-            if(gameBoard[item].every(checkPlayer2)) {
-                info.textContent = `${currentPlayer.player} Wins!`
+            // Each Column
+            if(getColumn1.every(checkPlayer) || getColumn2.every(checkPlayer) || getColumn3.every(checkPlayer)) {
+                wins.textContent = `${currentPlayer.player} Wins!`
+                winner = true
             }
-        }
-        if(gameBoard[0][0] === currentPlayer.mark && gameBoard[1][1] === currentPlayer.mark && gameBoard[2][2] === currentPlayer.mark) {
-            info.textContent = `${currentPlayer.player} Wins!`
-        }
-        if(gameBoard[0][2] === currentPlayer.mark && gameBoard[1][1] === currentPlayer.mark && gameBoard[2][0] === currentPlayer.mark) {
-            info.textContent = `${currentPlayer.player} Wins!`
+            // Cross Top Left to Bottom Right
+            if(gameBoard[0][0] === currentPlayer.mark && gameBoard[1][1] === currentPlayer.mark && gameBoard[2][2] === currentPlayer.mark) {
+                wins.textContent = `${currentPlayer.player} Wins!`
+                winner = true
+            }
+            // Cross Top Right to Bottom Left
+            if(gameBoard[0][2] === currentPlayer.mark && gameBoard[1][1] === currentPlayer.mark && gameBoard[2][0] === currentPlayer.mark) {
+                wins.textContent = `${currentPlayer.player} Wins!`
+                winner = true
+            }
+            // ITS A TIE!!
+            if(rounds === 9 && !winner) {
+                wins.textContent = "Well, nobody saw that coming! Its a tie!!"
+            }
+            if(winner) {
+                resetButton.style.opacity = '100%'
+                wins.style.fontSize = '80px'
+                info.style.opacity = '0%'
+            }
         }
     }
-    
+    function reset() {
+        winner = false
+        rounds = 0
+        currentPlayer = player1
+        info.innerHTML = currentPlayer.player
+        info.style.opacity = '100%'
+        resetButton.style.opacity = '0%'
+        wins.style.fontSize = '36px'
+        wins.textContent = " "
+        info.classList.toggle('animate')
+        gameBoard = [
+            ['','',''],
+            ['','',''],
+            ['','','']
+        ]
+        boxes.forEach(element => {
+            element.textContent = ""
+            element.style.backgroundColor = "white"
+        })
+    }
 })()
 
 export { Game }
